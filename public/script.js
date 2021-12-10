40e658fd7471f51f7ecb7492363c3f6eeb33f70d
arcanlte <pinangayjoel@gmail.com>
2021-12-09 19:27:08 -0500

    finalized

--- public/script.js
+++ public/script.js
@@ -1,86 +1,99 @@
-document.getElementById("clear").addEventListener("click",function(){
-    var div = document.getElementById("id")
-    while(div.lastElementChild){
-        div.removeChild(div.lastElementChild)
-    }
-    localStorage.clear()
-})
+document.getElementById("clear").addEventListener("click", function () {
+  var div = document.getElementById("id");
+  while (div.lastElementChild) {
+    div.removeChild(div.lastElementChild);
+  }
+  localStorage.clear();
+});
 
-function load(){
-    var div = document.getElementById("id");
-    var records = JSON.parse(localStorage.getItem("records"))
+function load() {
+  var records = JSON.parse(localStorage.getItem("records"));
 
-    for(let i = 0; i<records.length; i++){
-        createRecordDOM(records[i])
-    }
+  for (let i = 0; i < records.length; i++) {
+    createRecordDOM(records[i], i);
+  }
 }
 window.onload = load;
 
-function calculate(){
-    var radios = document.getElementsByName('tip');
-    var amount = document.getElementById("amount").value;
-    var location = document.getElementById("location").value;
+function calculate() {
+  var radios = document.getElementsByName("tip");
+  var amount = document.getElementById("amount").value;
+  var location = document.getElementById("location").value;
+  var date = new Date();
 
-    var obj = {
-        location: location,
-        amount: amount,
-        tip: 0,
-        amount_tip: 0
-    }
+  var obj = {
+    date: date.toLocaleDateString("en-US"),
+    time: date.toLocaleTimeString("en-US"),
+    location: location,
+    amount: amount,
+    tip: 0,
+    amount_tip: 0,
+  };
 
-    for (var i = 0, length = radios.length; i < length; i++) {
-        if (radios[i].checked) {
-            obj.tip = radios[i].value;
-            obj.amount_tip = ((1+radios[i].value)*amount).toFixed(2);
-            obj.tip = (obj.amount_tip-obj.amount).toFixed(2)
-            break;
-        }
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      obj.tip = radios[i].value;
+      obj.amount_tip = ((1 + radios[i].value) * amount).toFixed(2);
+      obj.tip = (obj.amount_tip - obj.amount).toFixed(2);
+      break;
     }
+  }
 
-    if(obj.length==0){
-        alert("Tip is required! Please choose tip option")
-    }
-    else{
+  if (obj.length == 0) {
+    alert("Tip is required! Please choose tip option");
+  } else {
     let records = JSON.parse(localStorage.getItem("records"));
-    if(records){
-        records.push(obj);
-        localStorage.setItem("records",JSON.stringify(records))
-    }else{
-        localStorage.setItem("records", JSON.stringify([obj]))
+    if (records) {
+      records.push(obj);
+      localStorage.setItem("records", JSON.stringify(records));
+    } else {
+      localStorage.setItem("records", JSON.stringify([obj]));
     }
 
-    createRecordDOM(obj);
-}
-}
+    if (records == undefined) {
+      records = [];
+    }
 
-function createRecordDOM(obj){
-    var container = document.createElement("div");
-    var location = document.createElement("p");
-    var amount = document.createElement("p");
-    var tip = document.createElement("p");
-    var amount_tip = document.createElement("p");
-    var separator = document.createElement("p");
-    var button = document.createElement("button");
-
-
-    location.textContent = "Store: " + obj.location;
-    amount.textContent = "Bill amount: $ " + obj.amount.toString();
-    tip.textContent = `Amount to tip: $ ${obj.tip}`
-    amount_tip.textContent = `Total amount: $ ${obj.amount_tip.toString()}`;
-    separator.textContent = "--------------------------"
-
-    
-
-    container.appendChild(location)
-    container.appendChild(amount)
-    container.appendChild(tip)
-    container.appendChild(amount_tip)
-    container.appendChild(separator)
-
-    document.getElementById("id").appendChild(container)
-
+    createRecordDOM(obj, records.length);
+  }
 }
 
+function createRecordDOM(obj, index) {
+  var container = document.createElement("div");
+  var date = document.createElement("p");
+  var time = document.createElement("p");
+  var location = document.createElement("p");
+  var amount = document.createElement("p");
+  var tip = document.createElement("p");
+  var amount_tip = document.createElement("p");
+  var separator = document.createElement("p");
+  var deleteButton = document.createElement("button");
+  deleteButton.setAttribute("id", index);
+  deleteButton.textContent = "REMOVE";
+  deleteButton.addEventListener("click", function () {
+    let records = JSON.parse(localStorage.getItem("records"));
+    let divList = document.getElementById("id");
+    records.splice(index, 1);
+    localStorage.setItem("records", JSON.stringify(records));
+    divList.removeChild(divList.childNodes[index + 1]);
+  });
 
+  date.textContent = "Date: " + obj.date;
+  time.textContent = "Time: " + obj.time;
+  location.textContent = "Store: " + obj.location;
+  amount.textContent = "Bill amount: $ " + obj.amount.toString();
+  tip.textContent = `Amount to tip: $ ${obj.tip}`;
+  amount_tip.textContent = `Total amount: $ ${obj.amount_tip.toString()}`;
+  separator.textContent = "--------------------------";
 
+  container.appendChild(date);
+  container.appendChild(time);
+  container.appendChild(location);
+  container.appendChild(amount);
+  container.appendChild(tip);
+  container.appendChild(amount_tip);
+  container.appendChild(deleteButton);
+  container.appendChild(separator);
 
+  document.getElementById("id").appendChild(container);
+}
